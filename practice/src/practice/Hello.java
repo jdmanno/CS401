f065895f2915536cc4a2d4af0fa71b85dd2413fa
jdmanno <jdmanno95@gmail.com>
2020-10-13 20:01:26 -0700

    Added Jonas Manno -- REMOVE

--- practice/src/practice/Hello.java
+++ practice/src/practice/Hello.java
@@ -3,5 +3,6 @@ package practice;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Prabin Newa -- REMOVE");
+		System.out.println("Jonas Manno -- REMOVE");
 	}
 }
